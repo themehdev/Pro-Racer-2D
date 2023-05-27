c7fc26622977345bce07430ea109b222c208c919
QuasarK <96966687+quasark-main@users.noreply.github.com>
2023-05-27 10:10:03 -0700

    Changed player movement

--- Player.gd
+++ Player.gd
@@ -5,6 +5,10 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var vel = Vector2(0, 0)
+var dir = Vector2(0, -1)
+var rotation_speed = 0.05
+var speed = 5
+var friction = 0.02
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,18 +17,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if is_on_wall():
-		vel.x = 0
 	if is_on_ceiling() or is_on_floor():
 		vel.y = 0
 	if Input.is_action_pressed("ui_up"):
-		vel.y -= 1
+		vel += Vector2.UP.rotated(dir.angle()) * speed
 	if Input.is_action_pressed("ui_down"):
-		vel.y += 1
+		vel += Vector2.DOWN.rotated(dir.angle()) * speed
 	if Input.is_action_pressed("ui_left"):
-		vel.x -= 1
+		dir = dir.rotated(-rotation_speed)
 	if Input.is_action_pressed("ui_right"):
-		vel.x += 1
+		dir = dir.rotated(rotation_speed)
+	
+	rotation = dir.angle()
+	
+	vel *= (1.0 - friction)
 
-	position += move_and_slide(vel)
+	move_and_slide(vel)
 	
